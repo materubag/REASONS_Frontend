bdedebde0e0de14e337c4714c77cff713e4f4b66
materubag <mrubio7466@uta.edu.ec>
2026-05-21 21:10:21 -0500

    actualiza shell y rutas de la aplicacion

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './features/home/pages/home-page/home-page.component';
+import { ProyectosPageComponent } from './features/proyectos/pages/proyectos-page/proyectos-page.component';
+import { ContactosPageComponent } from './features/contactos/pages/contactos-page/contactos-page.component';
+import { PublicacionesPage } from './features/publicaciones/pages/publicaciones-page/publicaciones-page';
+import { InvestigadoresPage } from './features/investigadores/pages/investigadores-page/investigadores-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomePageComponent,
+  },
+  {
+    path: 'proyectos',
+    component: ProyectosPageComponent,
+  },
+  {
+    path: 'investigadores',
+    component: InvestigadoresPage,
+  },
+  {
+    path: 'publicaciones',
+    component: PublicacionesPage,
+  },
+  {
+    path: 'contactos',
+    component: ContactosPageComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent, FooterComponent } from './layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, HttpClientModule, NavbarComponent, FooterComponent],
+  template: `
+    <div class="flex flex-col min-h-screen">
+      <app-navbar></app-navbar>
+      <main class="flex-grow">
+        <router-outlet></router-outlet>
+      </main>
+      <app-footer></app-footer>
+    </div>
+  `,
+  styleUrl: './app.scss',
 })
-export class App {
-  protected readonly title = signal('reasons-web');
-}
+export class App {}
